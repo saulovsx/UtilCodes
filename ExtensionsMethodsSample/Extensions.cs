7f9ec51c84972a56e3d963c3189a3d2506c81344
saulovsx <saulovsx@gmail.com>
2023-06-11 19:37:53 -0300

    New extension method "RemoveAccent". Return a new string without accents.

--- ExtensionsMethodsSample/Extensions.cs
+++ ExtensionsMethodsSample/Extensions.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Globalization;
 namespace ExtensionsMethodsSample
 {
     public static class Extensions
@@ -6,6 +8,15 @@ namespace ExtensionsMethodsSample
         {
             _ = DateTime.TryParse(value, out DateTime result);
             return result;
+        }
+
+        public static string RemoveAccent(this string text)
+        {
+            var textWithoutAccent = new string(text.Normalize(NormalizationForm.FormD)
+                                 .Where(ch => CharUnicodeInfo.GetUnicodeCategory(ch) != UnicodeCategory.NonSpacingMark)
+                                 .ToArray());
+            return textWithoutAccent;
         }        
+
     }
 }
--- ExtensionsMethodsSample/Program.cs
+++ ExtensionsMethodsSample/Program.cs
@@ -2,3 +2,6 @@
 
 string stringDate = "21/12/2023";
 Console.WriteLine($"The date is {stringDate.ToDate()}");
+
+string textAccent = "Café com Açucar";
+Console.WriteLine($"{textAccent}\n{textAccent.RemoveAccent()}");
